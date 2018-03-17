9d663c7a4fa3f180910979966549baf8a9d58266
Simon Lundström <simmel@soy.se>
2018-03-17 22:28:50 +0100

    Add positional path argument

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 #[macro_use]
 extern crate clap;
+use clap::Arg;
 
 fn main() {
   include_str!("../Cargo.toml");
-  let args = app_from_crate!().get_matches();
-  println!("{:?}", args);
+  let args = app_from_crate!()
+    .arg(
+      Arg::with_name("path")
+        .help("Path to Git repository")
+        .index(1)
+        .required(true),
+    )
+    .get_matches();
+
+  let path = args.value_of("path").unwrap();
+  println!("Git repo: {:?}", path);
 }
