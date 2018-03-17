420329173c2d1069b9bc58d8480e2676220b9e42
Simon Lundström <simmel@soy.se>
2018-03-17 23:06:22 +0100

    Add SSH_ORIGINAL_COMMAND from ENV
    We need to check that it exists for ourselves though.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate clap;
 use clap::Arg;
+use std::process;
 
 fn main() {
   include_str!("../Cargo.toml");
@@ -10,8 +11,22 @@ fn main() {
         .help("Path to Git repository")
         .required(true),
     )
+    .arg(
+      Arg::with_name("cmd")
+        .env("SSH_ORIGINAL_COMMAND")
+        .hidden(true),
+    )
     .get_matches();
 
   let path = args.value_of("path").unwrap();
+
+  match args.is_present("cmd") {
+    false => {
+      eprintln!("Error: SSH_ORIGINAL_COMMAND environment variable isn't set");
+      process::exit(1)
+    }
+    _ => (),
+  }
+
   println!("Git repo: {:?}", path);
 }
